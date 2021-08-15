b960a79faf74dfecbb61bab59370997807709ee2
Alexander Makarov <sam@rmcreative.ru>
2021-08-15 20:54:00 +0300

    Document StorageInterface

--- src/Storage/StorageInterface.php
+++ src/Storage/StorageInterface.php
@@ -4,20 +4,27 @@ declare(strict_types=1);
 
 namespace Yiisoft\Yii\RateLimiter\Storage;
 
+/**
+ * Counter storage.
+ */
 interface StorageInterface
 {
     /**
-     * @param string $key
-     * @param mixed $value
-     * @param int $ttl
+     * Persists counter value in the storage, uniquely referenced by a key with an optional expiration TTL time.
+     *
+     * @param string $key The ID of the counter to store.
+     * @param mixed $value The value of the counter.
+     * @param int $ttl The TTL value of this counter.
      */
     public function save(string $key, $value, int $ttl): void;
 
     /**
-     * @param string $key
-     * @param mixed $default
+     * Fetches a counter value from the storage.
      *
-     * @return mixed
+     * @param string $key The unique key of this counter in the storage.
+     * @param mixed $default Default value to return if the counter does not exist.
+     *
+     * @return mixed  The value of the counter from the storage, or $default in case of no counter with such key present.
      */
     public function get(string $key, $default = null);
 }
