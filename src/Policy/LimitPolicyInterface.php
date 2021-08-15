890e872a66e5fdcc351df87240c5b9df81544bc3
Alexander Makarov <sam@rmcreative.ru>
2021-08-15 20:02:31 +0300

    Update docs

--- src/Policy/LimitPolicyInterface.php
+++ src/Policy/LimitPolicyInterface.php
@@ -7,11 +7,20 @@ namespace Yiisoft\Yii\RateLimiter\Policy;
 use Psr\Http\Message\ServerRequestInterface;
 
 /**
- * Defines what to use to determine if the same client is making a hit when rate limiting.
- * For example, user ID will make rate limiting apply to current user only and an API token
- * will make rate limiting to apply to all users with the same token.
+ * Defines policy for limiting requests i.e. which requests should be hit-counted together and which should be
+ * counted separately.
+ *
+ * For example, a user ID-based fingerprint will make rate limiting apply to current user requests only. An API token-based
+ * fingerprint will make rate limiting to apply to all requests with the same token.
  */
 interface LimitPolicyInterface
 {
+    /**
+     * Returns request fingerprint. Two requests with the same fingerprint increase request counter. Requests
+     * with different fingerprints are counted and limited separately.
+     *
+     * @param ServerRequestInterface $request Request to get fingerprint for.
+     * @return string A fingerprint based on the request information.
+     */
     public function fingerprint(ServerRequestInterface $request): string;
 }
