142739b215cfe4bd9df0a78acaea3dd48ee5e9d8
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-08-09 18:01:39 +0300

    fix

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -10,4 +10,5 @@ return (new Configuration())
     ->setFileExtensions(['php'])
     ->addPathToScan(__DIR__ . '/src', isDev: false)
     ->addPathToScan(__DIR__ . '/tests', isDev: true)
-    ->ignoreErrorsOnPackages(['psr/http-message-implementation', 'ext-apcu'], [ErrorType::UNUSED_DEPENDENCY]);
+    // psr/http-message-implementation is a virtual package (implementation marker), it has no classes to detect usage of
+    ->ignoreErrorsOnPackage('psr/http-message-implementation', [ErrorType::UNUSED_DEPENDENCY]);
